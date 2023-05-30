177c5cc70ae06da139cf586ec9080e6948bc643a
Alexandre Arturi <alexandrea8@nycstudents.net>
2023-05-30 12:30:19 -0400

    5-30

--- src/MatrixSolver.java
+++ src/MatrixSolver.java
@@ -1,2 +1,131 @@
-package PACKAGE_NAME;public class MatrixSolver {
+public class MatrixSolver {
+
+    public static double[][] add(double[][] matrix1, double[][] matrix2) {
+        if (matrix1.length!=matrix2.length || matrix1[0].length!=matrix2[0].length) {
+            return null;
+        }
+
+        double[][] ret = new double[matrix1.length][matrix1[0].length];
+
+        for (int row = 0; row < matrix1.length; row++) {
+            for (int col = 0; col < matrix1[0].length; col++) {
+                ret[row][col] = matrix1[row][col] + matrix2[row][col];
+            }
+        }
+
+        return ret;
+    }
+
+    public static double[][] subtract(double[][] matrix1, double[][] matrix2) {
+        if (matrix1.length!=matrix2.length || matrix1[0].length!=matrix2[0].length) {
+            return null;
+        }
+
+        double[][] ret = new double[matrix1.length][matrix1[0].length];
+
+        for (int row = 0; row < matrix1.length; row++) {
+            for (int col = 0; col < matrix1[0].length; col++) {
+                ret[row][col] = matrix1[row][col] - matrix2[row][col];
+            }
+        }
+
+        return ret;
+    }
+
+    public static double[][] multiply(double[][] matrix1, double[][] matrix2) {
+        if (matrix1[0].length!=matrix2.length) {
+            return null;
+        }
+        double[][] ret = new double[matrix1.length][matrix2[0].length];
+        for (int row = 0; row < ret.length; row++) {
+            for (int col = 0; col < ret[0].length; col++) {
+                for (int i = 0; i < ret.length; i++) {
+                    ret[row][col] += matrix1[row][i]*matrix2[i][col];
+                }
+            }
+        }
+
+        for (int row = 0; row < ret.length; row++) {
+            for (int col = 0; col < ret[0].length; col++) {
+                ret[row][col]*=100;
+                ret[row][col] = (double)Math.round(ret[row][col])/100;
+            }
+        }
+        return ret;
+    }
+
+    public static boolean isInverse(double[][] matrix1, double[][] matrix2) {
+        double[][] product = multiply(matrix1,matrix2);
+        double[][] identity = new double[product.length][product[0].length];
+
+        for (int i = 0; i < identity.length; i++) {
+            if (i < identity[0].length) {
+                identity[i][i] = 1;
+            }
+            else {
+                break;
+            }
+        }
+
+        for (double[] row : identity) {
+            for (double col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+
+        for (int row = 0; row < identity.length; row++) {
+            for (int col = 0; col < identity[0].length; col++) {
+                if (product[row][col]!=identity[row][col]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static double[][] findInverse(double[][] matrix){
+        if (matrix.length != matrix[0].length || (matrix.length!=2 || matrix[0].length!=2)){
+            return null;
+        }
+
+        double[][] ret = new double[matrix.length][matrix.length];
+        if (ret.length==2){
+            ret[0][0] = matrix[1][1];
+            ret[0][1] = -1 * matrix[0][1];
+            ret[1][0] = -1 * matrix[1][0];
+            ret[1][1] = matrix[0][0];
+            double multiplier = (1.0)/(matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]);
+            for (int row = 0; row<ret.length; row++){
+                for (int col = 0; col<ret[0].length; col++){
+                    ret[row][col] *= multiplier;
+                }
+            }
+        }
+        for (int row = 0; row < ret.length; row++) {
+            for (int col = 0; col < ret[0].length; col++) {
+                ret[row][col]*=100;
+                ret[row][col] = (double)Math.round(ret[row][col])/100;
+            }
+        }
+        if (Math.abs(ret[0][0])>1000000) {
+            return null;
+        }
+        return ret;
+    }
+
+    public static double[][] scalarMultiplication(double scalar, double[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrix[row][col]*=scalar;
+            }
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrix[row][col]*=100;
+                matrix[row][col] = (double)Math.round(matrix[row][col])/100;
+            }
+        }
+        return matrix;
+    }
 }
--- src/matrixGUI.java
+++ src/matrixGUI.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class matrixGUI {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class matrixGUI extends JFrame implements ActionListener, KeyListener {
+    private JTextField rowField1;
+    private JTextField colField1;
+    private JButton submitButton;
+    private JLabel colLabel1;
+    private JLabel rowLabel1;
+    private JLabel introLabel;
+    private JPanel mainPanel;
+    private JTextField rowField2;
+    private JTextField colField2;
+    private JLabel rowLabel2;
+    private JLabel colLabel2;
+
+    public matrixGUI(){
+        createUIComponents();
+    }
+
+    private void createUIComponents(){
+        setContentPane(mainPanel);
+        setTitle("Matrix Calculator");
+        setSize(500,400);
+        setLocation(450,100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        submitButton.addActionListener(this);
+        colField1.addKeyListener(this);
+        rowField1.addKeyListener(this);
+        colField2.addKeyListener(this);
+        rowField2.addKeyListener(this);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        Object source = e.getSource();
+        if (source instanceof JButton){
+            int rows1 = Integer.parseInt(rowField1.getText());
+            int cols1 = Integer.parseInt(colField1.getText());
+            int rows2 = Integer.parseInt(rowField2.getText());
+            int cols2 = Integer.parseInt(colField2.getText());
+            System.out.println("Rows 1: " + rows1);
+            System.out.println("Columns 1: " + cols1);
+            System.out.println("Rows 2: " + rows2);
+            System.out.println("Columns 2: " + cols2);
+
+            JPanel second = new JPanel();
+            JTable matrix1 = new JTable();
+            JTable matrix2 = new JTable();
+            second.add(matrix1);
+            second.add(matrix2);
+            setContentPane(second);
+            revalidate();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+
+    }
 }
+
